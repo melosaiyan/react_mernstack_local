49cca6929314e650fd21cabb7e4340327428dba8
Brian Urbina <brian.urbina@outlook.com>
2020-10-09 09:02:43 -0500

    Updating all localhost references to use route hostname instead for axios

--- src/components/student-list.component.tsx
+++ src/components/student-list.component.tsx
@@ -14,7 +14,7 @@ export default class StudentList extends React.Component<any, any> {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:4000/students/')
+    axios.get('https://reactmernstacklocal-brian-test.devapps.c3ms.org:4000/students/')
       .then(res => {
         this.setState({
           students: res.data
