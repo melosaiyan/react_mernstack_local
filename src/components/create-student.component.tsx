adcfac9f39ce726799f631413d97630ab992a32c
Brian Urbina <brian.urbina@outlook.com>
2020-10-09 08:49:46 -0500

    Testing new hostname, NOW with HTTP, for axios backend

--- src/components/create-student.component.tsx
+++ src/components/create-student.component.tsx
@@ -44,7 +44,7 @@ export default class CreateStudent extends React.Component<any, any> {
       email: this.state.email,
       rollno: this.state.rollno
     };
-    axios.post('http://reactmernstacklocal.brian-test.svc.cluster.local/students/create-student', studentObject)
+    axios.post('https://reactmernstacklocal.brian-test.svc.cluster.local/students/create-student', studentObject)
       .then(res => console.log(res.data));
 
     this.setState({name: '', email: '', rollno: ''})
